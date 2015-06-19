de0181f1e1fb3f2345e5c72b5b38d152d3bd6083
ratanaksambath <ratanaksambath@gmail.com>
2015-06-19 05:09:52 -0700

    add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619002648) do
+ActiveRecord::Schema.define(version: 20150619111633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150619002648) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "chef_id"
+    t.string   "picture"
   end
 
 end
