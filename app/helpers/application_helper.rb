dd21e13ad97c1a4eb0c16b71d335ba5f7c2d25fa
ratanaksambath <ratanaksambath@gmail.com>
2015-06-19 04:04:44 -0700

    form and status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def gravatar_for(chef, option = { size: 100})
+    gravatar_id = Digest::MD5::hexdigest(chef.email.downcase)
+    size = option[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: chef.chefname, class: "gravatar")
+
+  end
+
 end
